aa92d8960e10555ecb1fea72e1a94cf2e2b451de
luis_ <luis_@192.168.1.38>
2020-06-08 13:44:10 +0200

    Pruebas de consulta en "PruebasServicioContacto" mediante método "findAll" con filtro pasadas.

--- src/main/java/com/vaadin/Pruebas/PruebasServicioContacto.java
+++ src/main/java/com/vaadin/Pruebas/PruebasServicioContacto.java
@@ -29,8 +29,9 @@ class PruebasServicioContacto {
 	{
 		this.serviciodatos = new ServicioContacto();
 		assertEquals(serviciodatos.findAll().size(),0);
-		Contacto contactoPrueba1 = new Contacto(null,"Gabrielle","Patel","Apple Inc.","PatelKalash@gmail.com","653842564","Calle Pantomima Full, 45");
-		Contacto contactoPrueba2 = new Contacto(null,"Pedro","Carlino","Microsoft Corporation","CarlinoPerrete@gmail.com","658942063","Calle Falsa, 123");
+		Contacto contactoPrueba1 = new Contacto(new Long(1),"Gabrielle","Patel","Apple Inc.","PatelKalash@gmail.com","653842564","Calle Pantomima Full, 45");
+		Contacto contactoPrueba2 = new Contacto(new Long(2),"Pedro","Carlino","Microsoft Corporation","CarlinoPerrete@gmail.com","658942063","Calle Falsa, 123");
+		Contacto contactoPrueba3 = new Contacto(new Long(3),"Pablo","Terrier","Microsoft Corporation","PabloPerrete@gmail.com","698731032","Calle Falsa, 122");
 		serviciodatos.save(contactoPrueba1);
 		assertEquals(serviciodatos.count(),1);
 		assertEquals(serviciodatos.findAll().size(),1);
@@ -38,6 +39,18 @@ class PruebasServicioContacto {
 		serviciodatos.save(contactoPrueba2);
 		assertEquals(serviciodatos.count(),2);
 		assertEquals(serviciodatos.findAll().size(),2);
+		serviciodatos.save(contactoPrueba3);
+		assertEquals(serviciodatos.count(),3);
+		assertEquals(serviciodatos.findAll().size(),3);
+		
+		assertEquals(serviciodatos.findAll("Gabrielle").size(),1);
+		assertTrue(serviciodatos.findAll("Gabrielle").get(0).equals(contactoPrueba1));
+		
+		assertEquals(serviciodatos.findAll("Carlino").size(),1);
+		assertTrue(serviciodatos.findAll("Carlino").get(0).equals(contactoPrueba2));
+		
+		assertEquals(serviciodatos.findAll("Microsoft Corporation").size(),2);
+		
 	}
 
 }
